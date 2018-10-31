fbede0e7627a7064b04a97f1af0558157d927c8f
vivekreddy95 <44635156+vivekreddy95@users.noreply.github.com>
2018-10-31 14:08:47 +0530

    java file created

---
+++ first.java
@@ -0,0 +1,14 @@
+class Hello {
+     
+     void display(){
+         System.out.println("hello world....");
+         }
+         }
+         
+         public class MainClass{
+         
+           public static void main(String args[]){
+              Hello o= new Hello();
+              o.display();
+              }
+              }
